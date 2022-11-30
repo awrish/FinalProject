9ea93c4ee9dd824c0d267040c0087fc3621a67f1
Awrish <awrishkhan@gmail.com>
2022-11-30 18:23:04 -0500

    Implemented add new list item button

--- app/src/main/java/edu/uga/cs/finalproject/NewListItemActivity.java
+++ app/src/main/java/edu/uga/cs/finalproject/NewListItemActivity.java
@@ -1,10 +1,70 @@
 package edu.uga.cs.finalproject;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
 
 public class NewListItemActivity extends AppCompatActivity {
 
     public static final String DEBUG_TAG = "NewJobLeadActivity";
+
+    private EditText itemNameView;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView(R.layout.activity_new_list_item);
+
+        itemNameView = findViewById(R.id.editText);
+
+        Button saveButton = findViewById(R.id.button);
+    }
+
+    private class ButtonClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            String itemName = itemNameView.getText().toString();
+
+            final ListItem listItem = new ListItem(itemName);
+
+            // Add a new element (ListItem) to the list of items in firebase
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference ref = database.getReference("shoppinglist");
+
+            ref.push().setValue( listItem )
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            // Show quick confirmation
+                            Toast.makeText(getApplicationContext(), "List Item created: " + listItem.getProductName(),
+                                    Toast.LENGTH_SHORT).show();
+
+                            // Clear editText for next use
+                            itemNameView.setText("");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), "Failed to create a list item for: " + listItem.getProductName(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
 
 
 
